b7b32f85041788a840bbbd379e14f36b780a1285
debkidder <wuttakidder@gmail.com>
2020-10-19 17:25:42 -0600

    improved-checkout-totals-test

--- test1_addtocart_checktotals.rb
+++ test1_addtocart_checktotals.rb
@@ -1,19 +1,37 @@
 ####
 # file: test1_addtocart_checktotals.rb
 # run from CLI: rspec test1_addtocart_checktotals.rb
+# Verifies totals on Payment Screen Page
+
+
 
 require "selenium-webdriver"
 require "rspec"
 
+$email_address = 'TestOctB2020@mailinator.com'
+$password = 'Tester123'
+$address = '123 Dot St.'
+$address2 = 'Apt 1'
+$city = 'Boise'
+#state = 'ID' Handled differently
+$zip = '83705'
+$country = 'United States'
+$phone = '2081112222'
+$ccnumber = '4111111111111111'
+$cardholder = 'Proud Mama'
+$expiry = '12/2023'
+$verification_code = '123'
+
+
 describe "Adding item to cart" do
-  it "results in correct totals at Checkout" do
+ it "results in correct totals at Checkout" do
 
     driver = Selenium::WebDriver.for :chrome
 
     # Starts on Product Page, add block set to cart (qty 1)
     driver.get 'https://lovevery-test.com/products/the-block-set'
 
-    # wait for button to show
+    # tinker with this waits
     wait = Selenium::WebDriver::Wait.new(timeout: 5)
     wait.until { driver.find_element(css: 'button[type="submit"]').displayed? }
 
@@ -34,15 +52,16 @@ describe "Adding item to cart" do
     driver.get 'https://lovevery-test.com/products/the-play-gym'
 
     # Wait for Play Gym product page
-    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    wait = Selenium::WebDriver::Wait.new(timeout: 25)
     #wait.until { driver.find_element(css: 'button[name="add"]').displayed? }
     wait.until { driver.find_element(xpath:  "//span[@data-add-to-cart-text]").displayed? }
 
     # On Play Gym product page, add to cart
+    #driver.find_element(css: 'button[name="add"]').click
     driver.find_element(xpath:  "//span[@data-add-to-cart-text]").click
 
     # In Cart
-    wait = Selenium::WebDriver::Wait.new(timeout: 5)
+    wait = Selenium::WebDriver::Wait.new(timeout: 15)
     wait.until { driver.find_element(xpath: "//button[@class='input-quantity-btn add cart-add-button']").displayed? }
 
     # Wait for Checkout button on Cart to load
@@ -72,18 +91,76 @@ describe "Adding item to cart" do
 
     # (Optional) After Checkout page loads, click checkbox to receive marketing info
     #driver.find_element(id: 'checkout_buyer_accepts_marketing').click
-
     # (Optional) To try to improve consistency
     driver.manage.timeouts.implicit_wait = 10
 
-    # Asserts correct subtotal
-expect(driver.find_element(xpath:  "//span[@data-checkout-subtotal-price-target='41000']"))
-puts ">>> PASS: Subtotal Test (actual subtotal matches expected subtotal)"
 
-    # Asserts correct total
-expect(driver.find_element(xpath:  "//span[@data-checkout-payment-due-target='41000']"))
-puts ">>> PASS: Total Test (actual total matches expected total)"
+    # Wait for Shipping Options form to show
+    wait = Selenium::WebDriver::Wait.new(timeout: 20)
+    #wait.until { driver.find_element(xpath: "//label[@for='checkout_shipping_address_address1']").displayed? }
+    wait.until { driver.find_element(xpath: "//input[@id='checkout_shipping_address_phone']").displayed? }
+
+    #=============================
+    # Because saved address seems to be cleared out daily
+
+    # Enter Address 1
+    driver.find_element(xpath: "//input[@id='checkout_shipping_address_address1']").send_keys($address)
+
+    # Enter City
+    driver.find_element(xpath: "//input[@id='checkout_shipping_address_city']").send_keys($city)
+
+    # Select state (aka province)
+    driver.find_element(xpath: "//option[@value='ID']").click
+
+    # Enter zip
+    driver.find_element(xpath: "//input[@id='checkout_shipping_address_zip']").send_keys($zip)
+
+    # Enter phone
+    driver.find_element(xpath: "//input[@id='checkout_shipping_address_phone']").send_keys($phone)
+
+    driver.manage.timeouts.implicit_wait = 5
+
+    # Click Continue to go to Shipping Method
+    driver.find_element(id: 'continue_button').click
 
 
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    #wait.until { driver.find_element(xpath: "//label[@for='checkout_shipping_address_address1']").displayed? }
+    wait.until { driver.find_element(xpath: "//div[@class='section section--shipping-method']").displayed? }
+
+    # Leave default shipping (in this test "Free Shipping")
+
+    # Click Continue to go to Payment screen
+    driver.find_element(id: 'continue_button').click
+
+    # Wait for Payment screen to load
+    wait = Selenium::WebDriver::Wait.new(timeout: 35)
+    #wait.until { driver.find_element(xpath: "//label[@for='checkout_shipping_address_address1']").displayed? }
+    wait.until { driver.find_element(xpath: "//div[@class='section section--payment-method']").displayed? }
+
+
+    # Passes if Payment Page subtotal matches hardcoded (for now) $410.00
+    expect(driver.find_element(xpath:  "//span[@data-checkout-subtotal-price-target='41000']"))
+    puts ">>> PASS: Payment Screen Subtotal (actual subtotal matches expected subtotal)"
+
+    # Passes if Payment Page tax matches hardcoded (for now) $24.60
+    expect(driver.find_element(xpath:  "//span[@data-checkout-total-taxes-target='2460']"))
+    puts ">>> PASS: Payment Screen Total (actual tax matches expected tax)"
+
+    # Passes if Payment Page total matches hardcoded (for now) $434.60
+    expect(driver.find_element(xpath:  "//span[@data-checkout-payment-due-target='43460']"))
+    puts ">>> PASS: Payment Screen Total (actual total matches expected total)"
+
+    #TROUBLESHOOTING CREDIT CARD FORM
+    # #driver.find_element(xpath: "//input[@id='number']").send_keys($ccnumber)
+    # driver.find_element(xpath: "//input[@name='name']").send_keys($cardholder)
+    # driver.find_element(xpath: "//input[@id='expiry']").send_keys($expiry)
+    # driver.find_element(xpath: "//input[@name='verification_value']").send_keys($verification_code)
+    #
+    # # Click Continue to go to Payment screen
+    # driver.find_element(id: 'continue_button').click
+    # driver.find_element(xpath: "//button[@name='button']").click
+
+   driver.quit
   end
-end
+ end
